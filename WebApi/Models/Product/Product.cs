2c7099d1af5c4b5d9ec2623cd936d4ae5ca9e1e6
Mustafa Balta <mustafa.balta@rnd.com.tr>
2023-09-30 18:03:43 +0300

    Add project files.

---
+++ WebApi/Models/Product/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Models.Product
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> ImageList { get; set; }
+        public decimal Price { get; set; }
+        public decimal ListPrice { get; set; }
+        public List<Specification> Specification { get; set; }
+    }
+}
---
+++ WebApi/Models/Product/Specification.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApi.Models.Product
+{
+    public class Specification
+    {
+        public string Name { get; set; }
+        public dynamic Value { get; set; }
+    }
+}
